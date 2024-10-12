62ce9eb36d6cbf981cf444c49307352e4c5a6f2d
Velikoborets <gtahzby1@gmail.com>
2024-10-12 17:51:19 +0300

    feat(js): fix script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,4 @@
-// $(document).ready(function() {
-//     console.log("JavaScript загружен и выполняется.");  // Проверка выполнения скрипта
-//     $("#pizzaForm").on('submit', function(event) {
-//         event.preventDefault();
-//         $.ajax({
-//             type: "POST",
-//             url: "ajaxHandler.php",
-//             data: $(this).serialize(),
-//             dataType: "json",
-//             success: function(response) {
-//                 if (typeof response.check !== "undefined" && response.check !== null) {
-//                     $("#check").html("Ваш заказ: " + response.check);
-//                 } else if (typeof response.error !== "undefined" && response.error !== null) {
-//                     $("#check").html("Ошибка: " + response.error);
-//                 } else {
-//                     $("#check").html("Неизвестная ошибка. Полный ответ сервера: " + JSON.stringify(response));
-//                 }
-//             },
-//             error: function(xhr, status, error) {
-//                 $("#check").html("Ошибка: " + error + "<br> Полный ответ сервера: " + xhr.responseText);
-//             }
-//         });
-//     });
-// });
-
-
-
 $(document).ready(function() {
-    console.log("JavaScript загружен и выполняется.");  // Проверка выполнения скрипта
     $("#pizzaForm").on('submit', function(event) {
         event.preventDefault();
         $.ajax({
